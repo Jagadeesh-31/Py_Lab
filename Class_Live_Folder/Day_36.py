f97b7194cd3bb2566a893d2850ad7ccc5881fb66
Jagadeesh Boyalla <22b91a6133srkracin@gmail.com>
2026-08-18 10:43:00 +0000

    Updated

---
+++ Class_Live_Folder/Day_36.py
@@ -0,0 +1,217 @@
+# Inheritannce --> Hierarchical Inheritance, Hybrid Inheritance 
+
+# Hierarchical Inheritance
+# whatsapp scenario
+'''
+class User:
+    """User class with Msg Propertites"""
+    def send_message(self):
+        print('Sending Messages')
+class PersonalUser(User):
+    """PErsonal user class inherting from user"""
+    def status_update(self):
+        print('Status Updated only for contacts')
+class BusinessUser(User):
+    """Business User"""
+    def creat_catlog(self):
+        print("catlog ccreation is possible")
+class VerifiedBusinessUser(User):
+    """Verified User"""
+    def premium_acess(self):
+        print("Bule tick added, premium features loaded")
+class CommunityUser:
+    """Commity Admi acess"""
+    def creat_commity(self):
+        print(f'Community Acess')
+user1 = User()
+user1.send_message()
+user2 = PersonalUser()
+user2.status_update()
+user3 = BusinessUser()
+user3.creat_catlog()
+user4 = CommunityUser()
+user4.creat_commity()
+'''
+
+
+# hybrid
+'''
+class User:
+    """User class with voice calls"""
+    def voice_call(self):
+        print("Making voice call")
+    def video_call(self):
+        print('making video call')
+class Notification(User):
+    """Sendind Notification"""
+    def notify(self):
+        print("Sending Notification")
+class BusinessUser:
+    """Business User acess"""
+    def catlog(self):
+        print('Catlog is Updated')
+class PremiumBusinessUsers(BusinessUser,Notification):
+    """Premium Content"""
+    def premium_acess(self):
+        print('Bule tick verification and reach acess')
+user1 = BusinessUser()
+user1.catlog()'''
+
+
+
+# Polymorphism
+
+# method overloadijng -->method with default argumrnts, method with varible len(*args), checkimg with varible type
+
+# method overiding --
+# oper overloading 
+
+# Hostar --> free user,premium user, adv premium user
+
+'''
+class Hostar:
+    """Simple example to understand ploymorphism"""
+    def watch(self):
+        print('Welcome to hotstar home page... and loading....')
+    def watch(self,movie):
+        self.movie = movie
+        print(f'Loaded hotstar watching {self.movie}')
+user = Hostar()
+user.watch("Leo")
+user.watch("89")
+'''
+
+
+# method overloading with default arguments
+'''
+class Hotstar:
+    """method overloading with default arguments"""
+    def watch(self,movie=None):
+        if movie==None:
+            print('Welcome to hotstar....')
+        else:
+            print(f"Watching {movie}")
+user = Hotstar()
+user.watch()
+user.watch("Vikram")
+'''
+
+# Inheritannce --> Hierarchical Inheritance, Hybrid Inheritance 
+
+# Hierarchical Inheritance
+# whatsapp scenario
+'''
+class User:
+    """User class with Msg Propertites"""
+    def send_message(self):
+        print('Sending Messages')
+class PersonalUser(User):
+    """PErsonal user class inherting from user"""
+    def status_update(self):
+        print('Status Updated only for contacts')
+class BusinessUser(User):
+    """Business User"""
+    def creat_catlog(self):
+        print("catlog ccreation is possible")
+class VerifiedBusinessUser(User):
+    """Verified User"""
+    def premium_acess(self):
+        print("Bule tick added, premium features loaded")
+class CommunityUser:
+    """Commity Admi acess"""
+    def creat_commity(self):
+        print(f'Community Acess')
+user1 = User()
+user1.send_message()
+user2 = PersonalUser()
+user2.status_update()
+user3 = BusinessUser()
+user3.creat_catlog()
+user4 = CommunityUser()
+user4.creat_commity()
+'''
+
+
+# hybrid
+'''
+class User:
+    """User class with voice calls"""
+    def voice_call(self):
+        print("Making voice call")
+    def video_call(self):
+        print('making video call')
+class Notification(User):
+    """Sendind Notification"""
+    def notify(self):
+        print("Sending Notification")
+class BusinessUser:
+    """Business User acess"""
+    def catlog(self):
+        print('Catlog is Updated')
+class PremiumBusinessUsers(BusinessUser,Notification):
+    """Premium Content"""
+    def premium_acess(self):
+        print('Bule tick verification and reach acess')
+user1 = BusinessUser()
+user1.catlog()'''
+
+
+
+# Polymorphism
+
+# method overloadijng -->method with default argumrnts, method with varible len(*args), checkimg with varible type
+
+# method overiding --
+# oper overloading 
+
+# Hostar --> free user,premium user, adv premium user
+
+'''
+class Hostar:
+    """Simple example to understand ploymorphism"""
+    def watch(self):
+        print('Welcome to hotstar home page... and loading....')
+    def watch(self,movie):
+        self.movie = movie
+        print(f'Loaded hotstar watching {self.movie}')
+user = Hostar()
+user.watch("Leo")
+user.watch("89")
+'''
+
+
+# method overloading with varible length argumnets
+'''
+class Hotstar:
+    """method overloading with default arguments"""
+    def watch(self,movie=None):
+        if movie==None:
+            print('Welcome to hotstar....')
+        else:
+            print(f"Watching {movie}")
+    def watching_list(self,*movie):
+        self.movie=movie
+        print(f'watching list movie {self.movie}')
+movie=input().lower()
+user = Hotstar()
+user.watch()
+user.watch("Vikram")
+user.watch(movie)
+user.watching_list(['89','Leo','salaar'])'''
+
+
+# method overloading --> checking types of argument
+# hotstar --> one movie,more movie...
+
+
+class Hotstar:
+    """checking type arguments usage """
+    def movie_list(self,content):
+        if isinstance(content,str):
+            print(f"watching {content}")
+        elif isinstance(content,list):
+            for movie in content:
+                print(movie)
+user = Hotstar()
+user.movie_list("vikram")
+user.movie_list(["Leo","Vikram","Salaar"])
---
+++ Class_Live_Folder/Day_37.py
@@ -0,0 +1,122 @@
+# ploymorphism
+
+# method overriding 
+
+# Hotstar  -->  free user (can watch limited content and advertisements)
+#           --->paid user (can watch premium content without advertisements)
+#           -->premium user (watch live and premium content without advertisements)
+'''
+class User:
+    """Method Overriding"""
+    def watch(self):
+        print("Watching basic content with advertisements")
+class PaidUser:
+    def watch(self):
+         print("Watching premium content without advertisements")
+    def paid_watch(self):
+                 print("Watching premium content without advertisements")
+u1 = User()
+u1.watch()
+u2 = PaidUser()
+u2.watch()
+u2.paid_watch()'''
+
+
+# Different Subscription Plans
+'''
+class User:
+    """Method Overriding with Hotstar scenario - Different Subscription Plans"""
+    def watch(self):
+        print("Watching basic content with advertisements")
+
+
+class VIP_User(User):
+    def watch(self):
+        super().watch()
+        print("Watching premium content without advertisements")
+
+
+class Premium_User(VIP_User):
+    def watch(self):
+        super().watch()
+        print("Watching premium Live content without advertisements")
+
+
+u1 = User()
+u1.watch()
+
+u2 = VIP_User()
+u2.watch()
+
+u3 = Premium_User()
+u3.watch()
+'''
+
+
+
+#Operator Overloading-->+,-,*,/
+#(Magic Method / Dunder Method)
+#__add__,__str__,__init__
+
+'''
+#integers
+a = 15;b = 23
+print(a+b)
+print(a.__add__(b))
+print(a.__add__(55))
+
+#string
+a = "jaga"
+b = "b"
+print(a+b)
+print(a.__add__(b))
+print(a.__str__())
+print(a.__add__("M"))
+
+#list
+a = [1, 2, 3]
+b = [4, 5, 6]
+print(a+b) #merging
+print(a.__add__(b))
+print(a.__len__()) #prints len(a)
+print(a.__add__([7, 8]))
+
+'''
+
+
+# Watch History in Hotstar Scenario
+'''
+class WatchHistroy:
+    """understanding watching"""
+    def __init__(self,hours):
+        self.hours=hours
+    # def new(self,other):
+        #return self.hour+other.hour  --> Type Error for + 
+    def __add__(self,other):
+        return self.hours+other.hours
+u1 = WatchHistroy(120)
+u2 = WatchHistroy(100)
+print(u1+u2)# here we are able to add both users watch history only beacuse 
+
+# we use __add__ ()
+'''
+
+
+
+class WatchHistroy:
+    """understanding watching"""
+    def __init__(self,hours):
+        self.hours=hours
+    # def new(self,other):
+        #return self.hour+other.hour  --> Type Error for + 
+    def __add__(self,other):
+        return self.hours+other.hours
+    def __str__(self):
+         return f'Watching content for {self.hours} hours'
+   # def new(self):
+      #   return f'Watching content for {self.hours} hours'
+
+u1 = WatchHistroy(120)
+u2 = WatchHistroy(100)
+print(u1+u2)
+print(u1.__str__())
